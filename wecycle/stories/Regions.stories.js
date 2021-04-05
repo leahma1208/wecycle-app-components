3d2d54b9ce0fd664ca57300a6e8dac53c115784a
Hyerin <hyerin.bcit@gmail.com>
2021-04-05 18:29:04 +0900

    hyerin comps

---
+++ wecycle/stories/Regions.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegionButton from '../comps/RegionButton';
+
+export default {
+    title:"Example/Button",
+    component:<RegionButton />
+}
+
+export const Button = () => <RegionButton
+text="Marine"
+/>
