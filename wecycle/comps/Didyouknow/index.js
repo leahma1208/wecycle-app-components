36f944bffc18335958bf4a594b6088f2bce5bbc3
KellyMenzul <kmenzul@my.bcit.ca>
2021-04-04 19:39:59 -0700

    kelly comps

---
+++ wecycle/comps/Didyouknow/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display:flex;
+    background-color: blue;
+    justify-content:left;
+    width:15em;
+    border-radius:19px;
+    background-color: ${props=>props.backcolor};
+`
+const Holder = styled.div `
+    display:flex;
+    flex-direction:column;
+    width:100%;
+`
+
+const HeaderHolder = styled.div `
+    background-color:red;
+    margin:15px;
+    background-color:${props=>props.headercolor};
+    border-radius:6px;
+    color:white;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+const InformationHolder = styled.div `
+    margin-left:5px;
+    paragraph-width:15em;
+    padding:0px 10px;
+    
+`
+
+const Didyouknow = ({
+    informationtext="Sugar plum carrot cake caramels jelly beans croissant cheesecake tart bonbon. Sugar plum carrot cake caramels jelly beans croissant ",
+    backcolor= "#E1E6FF",
+    headercolor= "#ADBAFF",
+
+}) => {
+    return (
+        <Container backcolor={backcolor}>
+            <Holder>
+                <HeaderHolder headercolor={headercolor}> 
+                    <h4>DID YOU KNOW?</h4>
+                </HeaderHolder>
+                <InformationHolder>
+                    <p>{informationtext}</p>
+                </InformationHolder>
+            </Holder>
+        </Container>
+    )
+}
+
+export default Didyouknow;
--- wecycle/pages/index.js
+++ wecycle/pages/index.js
@@ -1,12 +1,30 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import styled from 'styled-components';
 import Menu from '../comps/Menu';
+import Didyouknow from '../comps/Didyouknow';
+
+
+const Holder = styled.div `
+.container1 {
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items:center;
+}
+
+`
 
 export default function Home() {
-  return <div>
+  return <Holder>
+    <div className="container">
+    <Menu/>
+    </div>
+    <div className="container1">
+      <Didyouknow />
+    </div>
     
   
-    <Menu/>
-  </div>
+    
+  </Holder>
 
 }
