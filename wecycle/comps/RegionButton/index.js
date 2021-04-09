d4a1178537bd830c0b22c2c66e5f2ab294c8773d
Hyerin <hyerin.bcit@gmail.com>
2021-04-10 03:51:05 +0900

    Regions page

--- wecycle/comps/RegionButton/index.js
+++ wecycle/comps/RegionButton/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const RegionCont = styled.span`
-
+const RegionCont = styled.div`
+    margin-bottom:18px;
 `;
 
 const RegionInput = styled.button`
@@ -16,11 +16,13 @@ const RegionInput = styled.button`
 
 const RegionText= styled.p`
     color:#393939;
-    font-size:34px;
+    font-size:30px;
     font-weight:700;
     position:absolute;
     left:30px;
     font-family: 'Poppins', sans-serif;
+    z-index:10000;
+    margin-top: 46px;
 `;
 
 const Ellipse = styled.div`
@@ -31,14 +33,17 @@ const Ellipse = styled.div`
     position:absolute;
     right:29px;
     top:15px;
+    display:flex;
 `;
 
 const RegionImg = styled.img`
-    width:110px;
-    height:110px;
-    position:absolute;
-    top:31px;
-    right:25px;
+    width:${props=>props.width};
+    height:${props=>props.height};
+    margin-left: auto;
+    margin-right: auto;
+    margin-top:auto;
+    margin-bottom:auto;
+    
 `;
 
 const RegionButton = ({
@@ -46,14 +51,16 @@ const RegionButton = ({
     bgcolor="#ADBAFF",
     ellipsecolor="#E1E6FF",
     color="#393939",
-    src="/marine.svg"
+    src="/marine.svg",
+    width="110px",
+    height="110px"
 }) => {
 
     return <RegionCont>
         <RegionInput bg={bgcolor}>
             <RegionText color={color}>{text}</RegionText>
             <Ellipse ellipseBg={ellipsecolor}>
-                <RegionImg src={src}/>
+                <RegionImg src={src} width={width} height={height}/>
             </Ellipse>
         </RegionInput>
     </RegionCont>
---
+++ wecycle/pages/Region.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+import Menu from '../comps/Menu';
+import Header from '../comps/Header';
+import RegionButton from '../comps/RegionButton';
+
+const Holder = styled.div `
+.container1 {
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+};
+
+
+.hr {
+    border: 3px solid #E3E3E3;
+}
+`;
+
+export default function Regions() {
+    return <Holder>
+        <div>
+            <Menu></Menu>
+        </div>
+        <hr className="hr"/>
+        <div className ="container1">
+            <Header text="How is plastic Affecting  Different Regions?"/>
+        </div>
+        <div className="container1">
+            <RegionButton text="Marine"/>
+            <RegionButton text="Urban Cities" bgcolor="#AFDDCD" ellipsecolor="#C0ECDD" src="/UrbanCity.svg" width="93px" height="93px"/>
+            <RegionButton text="Landfills" bgcolor="#FFEAAD" ellipsecolor="#FFFBF0" src="/Landfills.svg" width="104px" height="104px" />
+        </div>
+
+    </Holder>
+}
